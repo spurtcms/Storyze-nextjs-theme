96c909e2254b383c5d36b322f365fb6ebd1b038f
Saravanan <piccotalent065@gmail.com>
2024-11-14 19:12:34 +0530

    design issue fix

--- src/app/post/[...slug]/layout.js
+++ src/app/post/[...slug]/layout.js
@@ -1,3 +1,4 @@
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,13 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
+   
     <html lang="en">
+      
       <body className={inter.className}>
           {children}
-
           <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
       </body>
     </html>
+    
   );
 }
+
